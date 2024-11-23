01d13e09490d33beb62f39758e9b79c44d9d58bc
Gustavo6900 <gustavopassianotocod@gmail.com>
2024-11-23 18:45:02 -0300

    arrumei
    lista produtos arrumado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,10 @@
-	
-import { useEffect, useState } from "react";
-import styles from "./components/home.module.css"
+
+import ListaProdutos from "../components/ListaProdutos"
 
 export default function Home(){
-    const [lista, setLista ] = useState([])
-
-        useEffect(() => {
-            const receberlistaprodutos = async () => {
-
-                try{
-                const resposta = await fetch('https://fakestoreapi.com/products')
-                const dados = await resposta.json();
-                setLista(dados)
-            }
-            catch{
-                alert("erro")
-            }
-        }
-        receberlistaprodutos()
-        }, [])
-        return(
-           <>
-           <h1>Lista Produtos</h1>
-
-           <ul className={styles.produto}> 
-            {lista.map(produto =>(
-                <li className={styles.item} key={produto.id}>
-                    <h2>{produto.title}</h2>
-                    <p>{produto.description}</p>
-                    <p> Preço: {produto.prico}</p>
-                    <img src={produto.image} alt={produto.title} width={100} />
-                </li>
-            ))}
-           </ul>
-           </>
-        )
-   
+return(
+    <di>
+        <ListaProdutos />
+    </di>
+)
 }
